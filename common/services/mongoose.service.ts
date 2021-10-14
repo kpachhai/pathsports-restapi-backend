a585c07d04f880aac552a5c6eda561f0c16e8b04
Waqas Ahmed <wakqasahmed@gmail.com>
2021-10-14 20:32:44 +0200

    Added configurable database

--- common/services/mongoose.service.ts
+++ common/services/mongoose.service.ts
@@ -3,11 +3,22 @@ import debug from 'debug';
 
 const log: debug.IDebugger = debug('app:mongoose-service');
 
+const MONGO_USERNAME = process.env.MONGO_USERNAME || 'mongoadmin';
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || 'mongopass';
+const MONGO_HOST = process.env.MONGO_HOST || 'localhost';
+const MONGO_PORT = Number(process.env.MONGO_PORT) || 37018;
+
 class MongooseService {
     private count = 0;
     private mongooseOptions = {
-        useNewUrlParser: true,
         useUnifiedTopology: true,
+        useNewUrlParser: true,
+        socketTimeoutMS: 30000,
+        keepAlive: true,
+        poolSize: 25,
+        autoIndex: false,
+        authSource: 'admin',
+        writeConcern: 'majority',
         serverSelectionTimeoutMS: 5000,
         useFindAndModify: false,
     };
@@ -23,7 +34,10 @@ class MongooseService {
     connectWithRetry = () => {
         log('Attempting MongoDB connection (will retry if needed)');
         mongoose
-            .connect('mongodb://localhost:27017/api-db', this.mongooseOptions)
+            .connect(
+                `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/api-db`,
+                this.mongooseOptions
+            )
             .then(() => {
                 log('MongoDB is connected');
             })
