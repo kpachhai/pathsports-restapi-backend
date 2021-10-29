10a95cdc4f487d38f22822369d1f81c2fea2c9fd
Kiran Pachhai <kiran.pachhai@gmail.com>
2021-10-29 19:20:45 +0000

    dotenv handling not needed

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,5 @@
 import dotenv from 'dotenv';
-const dotenvResult = dotenv.config();
-if (dotenvResult.error) {
-    throw dotenvResult.error;
-}
+dotenv.config();
 
 import express from 'express';
 import * as http from 'http';
